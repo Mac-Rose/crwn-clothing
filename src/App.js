a3fd9a4c4bde27441c6a177a04f02fed7924539a
Mac-Rose <phoenixstk17@gmail.com>
2022-12-03 10:47:16 +0530

    Added navigation and change menu-item component used spread operator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import HomePage from "./pages/homepage/homepage.component";
+import { Routes, Route } from "react-router";
+import { BrowserRouter, useLocation } from "react-router-dom";
+
+const HatsPage = () => {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <div>
+      <h1>HATSPAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/menu-item/menu-items.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./menu-items.styles.scss";
+import { useNavigate } from "react-router-dom";
+
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  let navigate = useNavigate();
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="content">
+        <h1 className="title"> {title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
