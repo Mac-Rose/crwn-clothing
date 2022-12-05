09a7818b040e171af8790cd94e2568fbf2ee1aad
Mac-Rose <phoenixstk17@gmail.com>
2022-12-05 09:08:23 +0530

    Created Shops page collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React from "react";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Routes, Route } from "react-router";
-import { BrowserRouter, useLocation } from "react-router-dom";
-
-const HatsPage = () => {
-  const location = useLocation();
-  console.log(location);
-  return (
-    <div>
-      <h1>HATSPAGE</h1>
-    </div>
-  );
-};
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </BrowserRouter>
   );
