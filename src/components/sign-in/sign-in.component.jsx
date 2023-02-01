02626ee1a86e27484fe91bcc3bae18a4f9f099b0
Mac-Rose <phoenixstk17@gmail.com>
2023-02-02 02:34:53 +0530

    finished completing sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -13,15 +13,22 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
   };
+
   render() {
     return (
       <div className="sign-in">
@@ -31,8 +38,8 @@ class SignIn extends React.Component {
           <FormInput
             type="email"
             name="email"
-            value={this.state.email}
             handleChange={this.handleChange}
+            value={this.state.email}
             label="E-Mail"
             required
           />
